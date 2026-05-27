eaf737ff16a6b8d269f39ebdca44cbcf604bd731
Izharkhan2022 <izhar.khan@softqubes.com>
2026-05-27 11:36:56 +0530

    cors chanegs

--- server/server.js
+++ server/server.js
@@ -18,7 +18,12 @@ const cartRoutes = require('./routes/cart')
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
